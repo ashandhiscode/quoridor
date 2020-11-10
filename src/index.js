20f9dc9210fdb943794586a4da143418e1950a27
ashandhiscode <73539201+ashandhiscode@users.noreply.github.com>
2020-11-10 11:15:10 +0000

    Add files via upload
    added index

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Arena from './components/Arena';
+
+ReactDOM.render(<Arena playerNames = {{1 : "Ash", 2: "Maan"}}/>, document.querySelector('#root'))
